8f31c09ec43bba07d8daa80ad8d7b925f1298f18
Mariem Moula <moula.meriame@gmail.com>
2024-09-28 09:31:40 +0100

    Proof of pushing the file to the local index

---
+++ src/main/java/tn/esprit/eventsproject/controllers/EventRestController.java
@@ -0,0 +1,42 @@
+package tn.esprit.eventsproject.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import tn.esprit.eventsproject.entities.Event;
+import tn.esprit.eventsproject.entities.Logistics;
+import tn.esprit.eventsproject.entities.Participant;
+import tn.esprit.eventsproject.services.IEventServices;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("event")
+@RestController
+public class EventRestController {
+    private final IEventServices eventServices;
+
+    @PostMapping("/addPart")
+    public Participant addParticipant(@RequestBody Participant participant){
+        return eventServices.addParticipant(participant);
+    }
+    @PostMapping("/addEvent/{id}")
+    public Event addEventPart(@RequestBody Event event, @PathVariable("id") int idPart){
+        return eventServices.addAffectEvenParticipant(event, idPart);
+    }
+    @PostMapping("/addEvent")
+    public Event addEvent(@RequestBody Event event){
+        return eventServices.addAffectEvenParticipant(event);
+    }
+    @PutMapping("/addAffectLog/{description}")
+    public Logistics addAffectLog(@RequestBody Logistics logistics,@PathVariable("description") String descriptionEvent){
+        return eventServices.addAffectLog(logistics,descriptionEvent);
+    }git
+    @GetMapping("/getLogs/{d1}/{d2}")
+    public List<Logistics> getLogistiquesDates (@PathVariable("d1") LocalDate date_debut, @PathVariable("d2") LocalDate date_fin){
+        System.out.println("MODIFYING THE PROJECT FOR GIT PURPOSE" + date_debut);
+        System.out.println("MODIFYING THE PROJECT FOR GIT PURPOSE 2" + date_debut);
+        return eventServices.getLogisticsDates(date_debut,date_fin);
+    }
+
+}
